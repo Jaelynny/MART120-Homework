594d17800e293b73aa1f67c6d03f0f2a91767e94
Jaelynny <jaelynn.youngrunningcrane@umconnect.umt.edu>
2023-12-11 21:42:09 -0700

    wk15

---
+++ Final/Homework 4/code.js
@@ -0,0 +1,15 @@
+onEvent("ChangeButton", "click", function( ) {
+  setScreen("Wendigo");
+});
+onEvent("ChangeB2", "click", function( ) {
+  setScreen("Kanon");
+});
+onEvent("CB5", "click", function( ) {
+  setScreen("LittlePeople");
+});
+onEvent("CB3", "click", function( ) {
+  setScreen("Sasquatch");
+});
+onEvent("Changebutton2", "click", function( ) {
+  setScreen("SkinWalker");
+});
